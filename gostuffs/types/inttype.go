8fd8679f24a38827aafc6423cfb2d824b92a4ca9
pratapsgit <pk1979m@gmail.com>
2020-08-20 23:21:49 +0530

    modified for byte

--- gostuffs/types/inttype.go
+++ gostuffs/types/inttype.go
@@ -7,12 +7,12 @@ import (
 var x int32
 var y rune
 var a uint8
-var b bool
+var b byte
 
 func main() {
 
 	fmt.Printf("rune type %T\n", y)
 	fmt.Printf("int32 type %T\n", x)
 	fmt.Printf("uint8 type %T\n", a)
-	fmt.Printf("bool type %T\n", b)
+	fmt.Printf("byte type %T\n", b)
 }
