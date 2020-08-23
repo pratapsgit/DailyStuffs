f0486bcf5106d94ee0c7750f5f0c66b22cf91a43
pratapsgit <pk1979m@gmail.com>
2020-08-23 10:22:28 +0530

    multipe ways of accessing

---
+++ gostuffs/functions/moreparams.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printData(e ...int){
+	fmt.Printf("Processing elements ")
+	for _, v := range e{
+		fmt.Printf(" %v", v)
+	}
+	fmt.Println()
+}
+
+func calculateSum(input ...int) (int, float64) {
+	var sum int
+	var average float64
+
+	printData(input...)
+
+	for _, v := range input {
+		sum += v
+	}
+
+	average = float64(sum) / float64(len(input))
+
+	return sum, average
+}
+
+func main() {
+	r1, r2 := calculateSum(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+	fmt.Printf("Sum : %v Average : %v\n", r1, r2)
+
+	xslice := []int{ 2, 3, 4, 5, 6, 7, 8 ,9}
+
+	r1, r2 = calculateSum(xslice...)
+
+	fmt.Printf("Sum : %v Average : %v\n", r1, r2)
+}
